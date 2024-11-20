28eebe1c16b91d6e09f1e3d3deb3cf582f2ed0cc
Jan Meissner <jxmeissner@gmail.com>
2024-11-20 12:39:17 +0100

    New Script folder

--- classifai/engine.py
+++ classifai/engine.py
@@ -7,12 +7,66 @@ from tqdm.auto import tqdm
 from typing import Dict, List, Tuple
 
 
-def train_step():
-    pass
+def train_step(
+    model: torch.nn.Module,
+    dataloader: torch.utils.data.DataLoader,
+    loss_fn: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+    device: torch.device,
+) -> Tuple[float, float]:
+    """Trains a PyTorch model for one epoch.
+
+    Passes a target PyTorch model through a DataLoader for one epoch,
+    calculating the loss and accuracy of the model.
+
+    Args:
+      model: A PyTorch model to be trained.
+      dataloader: A DataLoader instance for the model to be trained on.
+      loss_fn: A PyTorch loss function to calculate loss.
+      optimizer: A PyTorch optimizer to help minimize the loss function.
+      device: A target device to compute on (e.g. "cuda" or "mps" or "cpu").
+
+    Returns:
+      A tuple of the average loss and accuracy for the epoch.
+      (loss, accuracy)
+    """
+    model.train()
+    train_loss, train_acc = 0, 0
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+        y_pred = model(X)
+        loss = loss_fn(y_pred, y)
+        train_loss += loss.item()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+        train_acc += (y_pred_class == y).sum().item() / len(y_pred)
+    train_loss /= len(dataloader)
+    train_acc /= len(dataloader)
+    return train_loss, train_acc
 
 
-def test_step():
-    pass
+def test_step(
+    model: torch.nn.Module,
+    dataloader: torch.utils.data.DataLoader,
+    loss_fn: torch.nn.Module,
+    device: torch.device,
+) -> Tuple[float, float]:
+    test_loss, test_acc = 0, 0
+    model.eval()
+
+    with torch.inference_mode():
+        for batch, (X, y) in enumerate(dataloader):
+            X, y = X.to(device), y.to(device)
+            y_pred = model(X)
+            loss = loss_fn(y_pred, y)
+            test_loss += loss.item()
+            y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+            test_acc += (y_pred_class == y).sum().item() / len(y_pred)
+        test_loss /= len(dataloader)
+        test_acc /= len(dataloader)
+    return test_loss, test_acc
 
 
 def train(
@@ -57,3 +111,32 @@ def train(
     """
 
     results = {"train_loss": [], "train_acc": [], "test_loss": [], "test_acc": []}
+
+    for epoch in tqdm(range(epochs)):
+        train_loss, train_acc = train_step(
+            model=model,
+            dataloader=train_dataloader,
+            loss_fn=loss_fn,
+            optimizer=optimizer,
+            device=device,
+        )
+
+        test_loss, test_acc = test_step(
+            model=model,
+            dataloader=test_dataloader,
+            loss_fn=loss_fn,
+            device=device,
+        )
+        print(
+            f"Epoch: {epoch+1} | "
+            f"train_loss: {train_loss:.4f} | "
+            f"train_acc: {train_acc:.4f} | "
+            f"test_loss: {test_loss:.4f} | "
+            f"test_acc: {test_acc:.4f}"
+        )
+
+        results["train_loss"].append(train_loss)
+        results["train_acc"].append(train_acc)
+        results["test_loss"].append(test_loss)
+        results["test_acc"].append(test_acc)
+    return results
--- classifai/utils.py
+++ classifai/utils.py
@@ -6,6 +6,8 @@ import random
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
+from pathlib import Path
 
 
 def plot_transformed_images(image_paths, transform, n=3, seed=42):
@@ -50,3 +52,32 @@ def plot_one_image(image, image_class="No class given"):
     )
     plt.axis(False)
     plt.show()
+
+
+def save_model(model: torch.nn.Module, target_dir: str, model_name: str):
+    """Saves a PyTorch model to a target directory.
+
+    Args:
+      model: A target PyTorch model to save.
+      target_dir: A directory for saving the model to.
+      model_name: A filename for the saved model. Should include
+        either ".pth" or ".pt" as the file extension.
+
+    Example usage:
+      save_model(model=model_0,
+                 target_dir="models",
+                 model_name="05_going_modular_tingvgg_model.pth")
+    """
+    # Create target directory
+    target_dir_path = Path(target_dir)
+    target_dir_path.mkdir(parents=True, exist_ok=True)
+
+    # Create model save path
+    assert model_name.endswith(".pth") or model_name.endswith(
+        ".pt"
+    ), "model_name should end with '.pt' or '.pth'"
+    model_save_path = target_dir_path / model_name
+
+    # Save the model state_dict()
+    print(f"[INFO] Saving model to: {model_save_path}")
+    torch.save(obj=model.state_dict(), f=model_save_path)
